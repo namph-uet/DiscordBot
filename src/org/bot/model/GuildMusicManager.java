3c2af29666931854ffb2934f35354a3132c54277
namph-uet <hoangnamuet.vnu@gmail.com>
2020-11-23 23:25:16 +0700

    update

--- src/org/bot/model/GuildMusicManager.java
+++ src/org/bot/model/GuildMusicManager.java
@@ -8,7 +8,9 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 /**
  * Holder for both the player and a track scheduler for one guild.
@@ -27,8 +29,10 @@ public class GuildMusicManager {
      * Get private config
      */
     private final CrunchifyGetPropertyValues config;
+
     /**
      * Creates a player and a track scheduler.
+     *
      * @param manager Audio player manager to use for creating the player.
      */
     public GuildMusicManager(AudioPlayerManager manager) {
@@ -47,16 +51,22 @@ public class GuildMusicManager {
 
     public void searchOnYouTube(String searchKeyword) throws IOException {
         String keyword = searchKeyword.replace(" ", "+");
-        String URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=rating&q=" + keyword + config.getAPIkey();
-        Document doc = Jsoup.connect(URL).timeout(10 * 1000).get();
+        String URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + keyword + "&key=" + config.getAPIkey();
+        Document doc = Jsoup.connect(URL).timeout(10 * 1000).ignoreContentType(true).get();
 
         String getJson = doc.text();
         JSONObject jsonObject = null;
         try {
-            jsonObject = (JSONObject) new JSONTokener(getJson ).nextValue();
-            System.out.println(jsonObject.getString("videoId"));
-            System.out.println(jsonObject.getJSONObject("items").getString("videoId"));
-        } catch (JSONException e) {
+            jsonObject = (JSONObject) new JSONTokener(getJson).nextValue();
+            Object json2 = jsonObject.getJSONArray("items").get(0);
+            Class<?> clazz = json2.getClass();
+
+            Field field = clazz.getField("videoId");
+            Object fieldValue = field.get(json2);
+            System.out.println(fieldValue);
+        } catch (JSONException | NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
     }
